b576fc8b20d0e06a01176f72055059dce8f944bc
b.xu <baojun.xu@helsana.ch>
2021-08-02 17:26:21 +0200

    initial main function

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main() {
+    val scan = Scanner(System.`in`)
+    while (true) {
+        val line = scan.nextLine()
+        if (line == "q") {
+            break;
+        }
+        println(line)
+    }
+}
